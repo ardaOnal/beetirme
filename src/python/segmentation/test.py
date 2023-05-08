ff4146e2fba111f5002177f13749a67f15c5c35d
ardaOnal <arda.onal@ug.bilkent.edu.tr>
2023-05-08 16:55:04 +0300

    NLP Segment Anything Model

---
+++ src/python/segmentation/test.py
@@ -0,0 +1,21 @@
+from  PIL import  Image
+from lang_sam import LangSAM
+from lang_sam.utils import draw_image
+import numpy as np
+
+model = LangSAM(sam_type="vit_b")
+image_pil = Image.open('src/python/segmentation/grocery_tomato.png').convert("RGB")
+np_image = np.array(Image.open('src/python/segmentation/grocery_tomato.png'))
+text_prompt = 'red jello pudding box'
+masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
+
+print("Masks", masks)
+print("Boxes", boxes)
+print("Phrases", phrases)
+print("Logits", logits)
+
+result = draw_image(np_image, masks, boxes, phrases)
+print("Result")
+print(result)
+final_result = Image.fromarray(result)
+final_result.save('src/python/segmentation/grocery_tomato_result.jpeg', 'JPEG')
